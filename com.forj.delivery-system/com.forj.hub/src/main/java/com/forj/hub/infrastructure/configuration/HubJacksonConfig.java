9aeb1a7ad84c40d039c0d24bb3600eba7c64fc67
krystal-13 <honey9163@gmail.com>
2024-09-10 18:47:02 +0900

    [refactor] Spring Boot 의 ObjectMapper 기본 설정을 유지하면서, 커스터마이징 추가로 변경

--- com.forj.delivery-system/com.forj.hub/src/main/java/com/forj/hub/infrastructure/configuration/HubJacksonConfig.java
+++ com.forj.delivery-system/com.forj.hub/src/main/java/com/forj/hub/infrastructure/configuration/HubJacksonConfig.java
@@ -4,17 +4,17 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class HubJacksonConfig {
 
     @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
 
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
-        return objectMapper;
+        return builder
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .build();
     }
 }
 

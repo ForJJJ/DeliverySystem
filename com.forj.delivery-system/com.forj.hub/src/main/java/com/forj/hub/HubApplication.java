b017f4c1390a69c2627a3210211749a971343c75
krystal-13 <honey9163@gmail.com>
2024-09-15 21:34:22 +0900

    [refactor] Hub - common 모듈 의존성추가 및 SecurityUtil 추가로 관련 로직 변경

--- com.forj.delivery-system/com.forj.hub/src/main/java/com/forj/hub/HubApplication.java
+++ com.forj.delivery-system/com.forj.hub/src/main/java/com/forj/hub/HubApplication.java
@@ -3,10 +3,12 @@ package com.forj.hub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableFeignClients
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.forj.common", "com.forj.hub"})
 public class HubApplication {
 
 	public static void main(String[] args) {
